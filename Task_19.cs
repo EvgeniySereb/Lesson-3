7bc21e80e63b7ac1ff4f630e115446b8ca481ae7
Evgeniy <jenyasereb95@gmail.com>
2022-09-24 23:04:50 +0500

    1

---
+++ Task_19.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int Input()
+{
+    Console.Write("Введите пятизначное число : ");
+    int number =  Convert.ToInt32(Console.ReadLine());
+    var massiv = number.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+    return number;
+}    
+ void Check(int number)
+{
+    var massiv = number.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+    if (massiv!.Length == 5 )
+    {
+    
+      if (massiv[0] == massiv[4] && massiv[1] == massiv[3])
+{
+    Console.WriteLine($"{number} является палиандромом");
+}
+   else 
+{
+    Console.WriteLine($"{number} не является палиндромом");
+}
+     
+     } 
+        else 
+     {
+        Console.WriteLine("Введено неверное число");
+
+     }
+     
+
+}
+ int number =Input();
+ Check(number);
